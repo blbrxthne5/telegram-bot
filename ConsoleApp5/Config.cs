78d4f49cfb8fca76a5c2a4fed058ef1ad65fec76
Lev <58858416+blbrxthne5@users.noreply.github.com>
2021-03-30 18:54:35 +0300

    hhhbot

---
+++ ConsoleApp5/Config.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learnTelegramBot
+{
+    public static class Config
+    {
+        public static string Token { get; set; } = "1637920843:AAFR-OflEJemGEY9odrn9ktsuE0o6sGtYWs";
+    }
+}
